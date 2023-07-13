84cad0a4a15ea7b5fb518b09e04bc9f741500703
RiskevO3 <91577090+RiskevO3@users.noreply.github.com>
2023-07-13 23:53:37 +0700

    .

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,7 +2,7 @@
   <section class="dark:bg-transparent space-y-0">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <div
-        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+        class="w-full rounded-lg shadow border dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 dark:bg-gray-800 border-gray-700 dark:border-gray-700"
       >
         <form class="p-6 space-y-4 md:space-y-6 sm:p-8" @submit.prevent="login">
           <h1
@@ -14,7 +14,7 @@
             <div>
               <label
                 for="username"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                class="block mb-2 text-sm font-medium text-white dark:text-white"
                 >
                 Username
                 </label
@@ -23,7 +23,8 @@
                 type="username"
                 name="username"
                 id="username"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:disabled:bg-gray-700 dark:disabled:text-gray-400"
+                class="
+                bg-gray-700 border border-gray-600 text-white placeholder-gray-400 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 :disabled:bg-gray-700 :disabled:text-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:disabled:bg-gray-700 dark:disabled:text-gray-400"
                 placeholder="username"
                 v-model="username"
               />
@@ -31,7 +32,7 @@
             <div>
               <label
                 for="password"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                class="block mb-2 text-sm font-medium text-white dark:text-white"
                 >Password
                 </label
               >
@@ -40,7 +41,8 @@
                 name="password"
                 id="password"
                 placeholder="••••••••"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:disabled:bg-gray-700 dark:disabled:text-gray-400"
+                class="bg-gray-700 border border-gray-600 text-white placeholder-gray-400 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-primary-600 block w-full p-2.5 :disabled:bg-gray-700 :disabled:text-gray-400
+                dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-primary-600 dark:disabled:bg-gray-700 dark:disabled:text-gray-400"
                 v-model="password"
                 />
             </div>
@@ -49,13 +51,14 @@
             </div>
             <button
               type="submit"
-              class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-info dark:hover:bg-success dark:disabled:hover:bg-info dark:disabled:opacity-50"
-            >
+              class="w-full text-white bg-info hover:bg-success focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:opacity-50 disabled:cursor-not-allowed
+                    dark:bg-info dark:hover:bg-success dark:disabled:hover:bg-info dark:disabled:opacity-50"
+              >
               Sign in
             </button>
-            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+            <p class="text-sm font-light text-gray-400 dark:text-gray-400">
               Don’t have an account yet?
-              <RouterLink to="/register" class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+              <RouterLink to="/register" class="font-medium text-primary-500 hover:underline dark:text-primary-500"
                 >Sign up
               </RouterLink
               >
